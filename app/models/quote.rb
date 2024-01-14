2306d72529bc26602a386ccaae2e9278c7616341
Nay Lin Aung <naylinaung@istd.co.jp>
2024-01-14 21:35:29 +0630

    feat():=> add devise gem

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
     validates :name, presence: true
 
     scope :ordered, -> { order(id: :desc) }
+
+    belongs_to :company
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes
   resources :poem_books
+  devise_for :users
   # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root to: "pages#home"
 end
